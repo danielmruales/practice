e1361bbcf04ad826afbbca84e3d2661fc2b0962e
Daniel Ruales <danielmruales@gmail.com>
2019-08-13 21:25:18 -0600

    added react awesome button

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar.js'
+import {AwesomeButton } from 'react-awesome-button';
+import {Link} from 'react-router-dom'
 import './Home.css'
+import 'react-awesome-button/dist/styles.css'
+
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
-            <h1 className='title' > <span className='arrow'>&#10094;</span> <span className='h'>&#8460;</span> ello Wolrddd<span>&#9884;</span> <span className='rightArrow'>&#10095;</span> </h1>
+            <div className='title'> 
+                <h1> <span className='arrow'>&#10094;</span> <span className='h'>&#8460;
+                    </span> ello Wolrddd<span>&#9884;</span> 
+                    <span className='rightArrow'>&#10095;</span> 
+                </h1>
+                <Link to='/react'><AwesomeButton type="primary" >Cool Button!</AwesomeButton></Link>
+         
+            </div>
+           
         </div>
     );
 };
