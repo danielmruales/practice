2772ebbfbfd48da68f728df4741eb66b1d6e5125
ryanbliss <smile@ryanbliss.me>
2019-08-31 15:52:49 -0600

    Deleted and renamed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
-import FormWidget from './React/FormWidget.js'
+import FormWidget from './FormWidget/FormWidget.js'
 import './App.css';
 
 function App() {
