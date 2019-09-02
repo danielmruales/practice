4a5fd5e92b9d67a5e400f261fac9c507b0d84f3f
Daniel Ruales <danielmruales@gmail.com>
2019-09-01 21:51:27 -0600

    uninstalled react router dom and awesome button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
 import FormWidget from './FormWidget/FormWidget.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Switch>
-      <Route exact path='/' component={FormWidget}/>
-    </Switch>
+      <FormWidget/>
     </div>
   );
 }
--- src/FormWidget/FormWidget.js
+++ src/FormWidget/FormWidget.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-// import Navbar from '../Navbar/Navbar.js'
 import TextField from './inputs/TextField/TextField.js';
 import FilePicker from './inputs/FilePicker/FilePicker.js';
 import "./FormWidget.css";
